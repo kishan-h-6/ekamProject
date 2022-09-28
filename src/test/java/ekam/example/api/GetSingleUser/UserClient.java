fe4197b70b5c56b732ddf0148016abc2b8d5a849
Kishan H <testvagrant@TestVagrants-MacBook-Pro.local>
2022-09-28 15:28:45 +0530

    timer

--- src/test/java/ekam/example/api/GetSingleUser/UserClient.java
+++ src/test/java/ekam/example/api/GetSingleUser/UserClient.java
@@ -25,6 +25,8 @@ public class UserClient extends RetrofitBaseClient {
         service = httpClient.getService(UserService.class);
     }
 
+
+
     @APIStep(keyword = "When", description = "I invoke getSingleUser API")
     public GetSingleUserResponse getSingleUser(int userId) {
         timeTracker time = new timeTracker();
